8054a86e1e71b9473eff4b708dab7ef6ecc3c453
Peter Neely <peter.neely@sony.com>
2018-10-25 08:53:26 -0700

    spring-boot

---
+++ src/main/java/com/neely/udemyspringpetclinic/UdemySpringPetClinicApplication.java
@@ -0,0 +1,12 @@
+package com.neely.udemyspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemySpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UdemySpringPetClinicApplication.class, args);
+    }
+}
